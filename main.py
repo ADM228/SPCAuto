734cc983e30d7d6d81607fa379308b2c6df00cb9
ADM228 <msh.alexgoog@gmail.com>
2022-06-14 19:54:56 +0300

    wvcenter.py

--- main.py
+++ main.py
@@ -6,15 +6,26 @@ directory = ""
 dirlevel = 2
 unit = 100 #ms
 
+echodet = input ("Detect echo? [Y/N]: ")
+echodet = True if echodet == "y" or echodet == "Y" else False
+
+pad = input ("Pad the tracks? [Y/N]: ")
+pad = True if pad == "y" or pad == "Y" else False
+
 for i in range (dirlevel):
     i = 0
     dirs = []
-    print("Input the number to select the directory (0 to cancel):")
     for entry in os.scandir(directory):
         if entry.is_dir():
             i+=1
-            print("(" + str(i) + ") " + entry.name)
             dirs.append(entry.name)
+    if len(dirs) == 1:
+        directory += dirs[0] + "/"
+        print("Found only one directory, progressing automatically")
+        continue
+    print("Input the number to select the directory (0 to cancel):")
+    for i in range(len(dirs)):
+        print("("+str(i+1)+") "+dirs[i])
     dirnum = int(input ("The number (0-"+str(i)+"): "))
     if dirnum == 0:
         sys.exit("Cancelled")
@@ -22,34 +33,41 @@ for i in range (dirlevel):
         directory += dirs[dirnum-1] + "/"
 directory += input("Input the beginning of file names: ") + "_"
 paramstr = ""
-for i in range (7,int(input("Input the amount of tracks: "))+1):
+for i in range (1,int(input("Input the amount of tracks: "))+1):
     sr1, w1 = wav.read(directory + str(i) + ".wav")
     print ("Track", str(i)+":1 loaded successfully!")
-    sr2, w2 = wav.read(directory + str(i) + "e.wav")
-    print ("Track", str(i)+":2 loaded successfully!")
-    zeramount = int(input("Input the padding in units ("+str(unit)+"ms): "))
-    if zeramount != 0:
-        zer = np.zeros((int(sr1*unit*zeramount*0.001),2),np.float32)
-        w1 = np.vstack((zer,w1))
-        w2 = np.vstack((zer,w2))
-    print ("Track "+str(i)+" padded successfully!")
-    shape = w1.shape[0] - w2.shape[0]
-    if shape > 0:
-        zer = np.zeros((shape,2))
-        w2 = np.vstack((zer,w2))
-    elif shape < 0:
-        zer = np.zeros((abs(shape),2))
-        w1 = np.vstack((zer,w1))
-    w3 = w1 - w2
-    echo = w3.any()
-    print ("Echo of", i, "calculated successfully, it "+("has" if echo else "doesn't have")+" echo!")
+    if echodet:
+        sr2, w2 = wav.read(directory + str(i) + "e.wav")
+        print ("Track", str(i)+":2 loaded successfully!")
+    if pad:
+        zeramount = int(input("Input the padding in units ("+str(unit)+"ms): "))
+        if zeramount != 0:
+            zer = np.zeros((int(sr1*unit*zeramount*0.001),2),np.float32)
+            w1 = np.vstack((zer,w1))
+            if echodet:
+                w2 = np.vstack((zer,w2))
+        print ("Track "+str(i)+" padded successfully!")
+    if echodet:
+        shape = w1.shape[0] - w2.shape[0]
+        if shape > 0:
+            zer = np.zeros((shape,2))
+            w2 = np.vstack((zer,w2))
+        elif shape < 0:
+            zer = np.zeros((abs(shape),2))
+            w1 = np.vstack((zer,w1))
+        w3 = w1 - w2
+        echo = w3.any()
+        print ("Echo of", i, "calculated successfully, it "+("has" if echo else "doesn't have")+" echo!")
     stereo = ((w1[:,1]) - (w1[:,0])).any()
     print("Track "+str(i)+" is "+("stereo!" if stereo else "mono!"))
-    wav.write(directory + str(i) + ".wav", sr1, w2)
-    if echo:
-        wav.write(directory + str(i) + "e.wav", sr1, w3)
+    if echodet:
+        wav.write(directory + str(i) + ".wav", sr1, w2)
+        if echo:
+            wav.write(directory + str(i) + "e.wav", sr1, w3)
+        else:
+            os.remove(directory + str(i) + "e.wav")
     else:
-        os.remove(directory + str(i) + "e.wav")
+        wav.write(directory + str(i) + ".wav", sr1, w1)
     print("Files #"+str(i), "written sucessfully!")
-    paramstr +=(str(i)+": "+("E" if echo else "-")+("S" if stereo else "M")+"\n")
+    paramstr +=(str(i)+": "+("" if not echodet else "E" if echo else "-")+("S" if stereo else "M")+"\n")
 print(paramstr)
---
+++ wvcenter.py
@@ -0,0 +1,112 @@
+import numpy as np
+import scipy.io.wavfile as wav
+import os, sys
+import curses
+
+def getUnit():
+    a = int(input("Input the unit of space where averaging happens (0 to cancel):\n(1) Samples (relative to input sample rate)\n(2) Lowest frequency in the input file\n(3) Lowest note in the input file\nNumber (0-3): "))
+    if a == 0:
+        sys.exit("Cancelled")
+    elif a == 1:
+        return (int(input("Input the amount in samples: ")), 0)
+    elif a == 2:
+        return (int(input("Input the frequency: ")), 1)
+    elif a == 3:
+        b = int(input("Input the octave of the note (-2 to cancel, 4 being the Middle C): "))*12
+        if b == -24:
+            sys.exit("Cancelled")
+        c = int(input("Input the number of the note (0 to cancel, 1 - C, 12 - B/H): "))-1
+        if c == -1:
+            sys.exit("Cancelled")
+        b += c
+        c = b - 57
+        return (440*(2**(c/12)),1)
+
+directory = ""
+dirlevel = 2
+
+unit, f = getUnit()
+offset = 4 #samples per try
+algorithm = int( input ("Select the centering algorithm (0 to cancel):\n(1) Averaging\n(2) Min-max\nThe number (0-2): "))
+if algorithm == 0:
+    sys.exit("Cancelled")
+algorithm-=1
+
+for i in range (dirlevel):
+    i = 0
+    dirs = []
+    for entry in os.scandir(directory):
+        if entry.is_dir():
+            i+=1
+            dirs.append(entry.name)
+    if len(dirs) == 1:
+        directory += dirs[0] + "/"
+        print("Found only one directory, progressing automatically")
+        continue
+    print("Input the number to select the directory (0 to cancel):")
+    for i in range(len(dirs)):
+        print("("+str(i+1)+") "+dirs[i])
+    dirnum = int(input ("The number (0-"+str(i)+"): "))
+    if dirnum == 0:
+        sys.exit("Cancelled")
+    else:
+        directory += dirs[dirnum-1] + "/"
+directory += input("Input the file name: ")
+print("\n")
+
+sr1, w1 = wav.read(directory)
+
+if f == 1:
+    unit = sr1 / unit
+unit *= 1.2
+
+w1l = (w1[:,0])
+
+w_intermediate = np.empty(0,w1l.dtype) 
+
+w1fl = np.empty(w1l.shape,w1l.dtype)
+
+for i in range(0, int(w1l.shape[0]/offset)):
+    first_index = i*offset-int(unit/2)
+    first_index = first_index if first_index >= 0 else 0
+    w_intermediate = w1l[first_index:i*offset+int(unit/2)]
+    if algorithm == 0:
+        w_im_avg = np.cast[w1l.dtype](np.average(w_intermediate))
+    else: 
+        ma = max(w_intermediate)
+        mi = min(w_intermediate)
+        w_im_avg = np.cast[w1l.dtype]((ma+mi)/2)
+
+    #print(w_intermediate[0], end=" ")
+    w_intermediate = w1l[first_index:i*offset+int(unit/2)] - w_im_avg
+    w1fl[i*offset:(i+1)*offset] = w_intermediate[0:offset]
+    #print(w_im_avg, w_intermediate[0])
+    print ("\033[F"+str(i+1)+"/"+str(w1l.shape[0]/offset))
+
+w1r = (w1[:,1])
+
+w1fr = np.empty(w1r.shape,w1r.dtype)
+
+for i in range(0, int(w1r.shape[0]/offset)):
+    first_index = i*offset-int(unit/2)
+    first_index = first_index if first_index >= 0 else 0
+    w_intermediate = w1r[first_index:i*offset+int(unit/2)]
+    if algorithm == 0:
+        w_im_avg = np.cast[w1r.dtype](np.average(w_intermediate))
+    else: 
+        ma = max(w_intermediate)
+        mi = min(w_intermediate)
+        w_im_avg = np.cast[w1r.dtype]((ma+mi)/2)
+
+    #print(w_intermediate[0], end=" ")
+    w_intermediate = w1r[first_index:i*offset+int(unit/2)] - w_im_avg
+    w1fr[i*offset:(i+1)*offset] = w_intermediate[0:offset]
+    #print(w_im_avg, w_intermediate[0])
+    print ("\033[F"+str(i+1)+"/"+str(w1r.shape[0]/offset))
+
+w1f = np.empty(w1.shape,w1.dtype)
+
+w1f[:,0] = w1fl
+w1f[:,1] = w1fr
+wav.write(directory, sr1, w1f)
+
