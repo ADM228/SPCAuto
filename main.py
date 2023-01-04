f43ced1314452055cab2319a48b279567547eb71
ADM228 <msh.alexgoog@gmail.com>
2023-01-04 18:59:55 +0300

    Amplification

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ echosep = False
 if echodet:
     echosep = input ("Separate the echo? [Y/N]: ")
     echosep = True if echosep == "y" or echosep == "Y" else False
-# Automatic echo separation is disabled because it is bullshit for some reason
+
 
 pad = input ("Pad the tracks? [Y/N]: ")
 pad = True if pad == "y" or pad == "Y" else False
@@ -58,20 +58,32 @@ if index == True:
 else:
     nrange = range (1, i)
 
-
 for i in nrange:
     zeramount = ""
     sr1, w1 = wav.read(directory + str(i) + ".wav")
+    d1 = w1.dtype
+    if "int" in str(d1):
+        max1 = np.iinfo(d1).max
+        min1 = np.iinfo(d1).min
+    elif "float" in str(d1):
+        max1 = np.finfo(d1).max
+        min1 = np.finfo(d1).min
+    else:
+        print ("I don't know what a '"+str(d1)+"' is, please report to alexmush#7063 on Discord")
+    
     print ("Track", str(i)+":1 loaded successfully!")
     if echodet:
         sr2, w2 = wav.read(directory + str(i) + "e.wav")
+        d2 = w2.dtype
+        if d2 != d1:
+            print ("Why is your echo exported in a different format than the main file? Using the main file's type properties anyway")
         print ("Track", str(i)+":2 loaded successfully!")
     if pad:
         while not zeramount.isdigit():
             zeramount = input("Input the padding in units ("+str(unit)+"ms): ")
         zeramount = int(zeramount)
         if zeramount != 0:
-            zer = np.zeros((int(sr1*unit*zeramount*0.001),2),np.float32)
+            zer = np.zeros((int(sr1*unit*zeramount*0.001),2),d1)
             w1 = np.vstack((zer,w1))
             if echodet:
                 w2 = np.vstack((zer,w2))
@@ -87,15 +99,8 @@ for i in nrange:
         w3 = w1 - w2
         echo = w3.any()
         print ("Echo of", i, "calculated successfully, it "+("has" if echo else "doesn't have")+" echo!")
-    if echosep:
-        w10 = w1[:,0]
-        w11 = w1[:,1]
-        w20 = w2[:,0]
-        w21 = w2[:,1]
-        w30 = w10 - w20
-        w31 = w11 - w21
-        w3[:,0] = w30
-        w3[:,1] = w31
+    else:
+        echo = False
     if echo:
         stereo = ((w2[:,1]) - (w2[:,0])).any()
         print("Track "+str(i)+" is "+("stereo!" if stereo else "mono!"))
@@ -114,7 +119,15 @@ for i in nrange:
         wav.write(directory + str(i) + ".wav", sr1, w1)
     print("Files #"+str(i), "written sucessfully!")
     if echodet:
-        paramstr +=(str(i)+": "+("S" if stereo else "M")+("" if not echodet else "E" if echo else "-")+("" if not echo else "S" if estereo else "M")+"\n")
+        paramstr +=(str(i)+": "+("Stereo; " if stereo else "Mono;   ")+("" if not echodet else "E" if echo else "-")+(" " if not echo else "Stereo; " if estereo else "Mono;   "))
     else:
-        paramstr +=(str(i)+": "+("" if not echodet else "E" if echo else "-")+"\n")
+        paramstr +=(str(i)+": "+("Stereo; " if stereo else "Mono;   "))
+    if echosep:
+        paramstr +="Main Amp: "+(str(min(max1/np.amax(w2), min1/np.amin(w2))))+"; Echo Amp: "+(str(min(max1/np.amax(w3), min1/np.amin(w3)) if echo else "")+" ")
+    else:
+        paramstr +="Amp: " + (str(min(max1/np.amax(w1), min1/np.amin(w1))))
+#    if stereo:
+#        paramstr +=(str(np.amax((w2[:,1])-(w2[:,0]
+# Stereo difference, pointless
+    paramstr += "\n"
 print(paramstr)
